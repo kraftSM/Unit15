ecc25f3f5d58edd558b9fe4e00a41b30d3e31788
kraftSM <kraftsm@yandex.ru>
2023-04-11 16:14:13 +0300

    Добавьте файлы проекта.

---
+++ Unit15.Hw/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit15.Hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+            Console.WriteLine(string.Join(" ", allStudents));
+
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var studentNames = from cl in classes
+                               from student in cl.Students
+                                   //orderby cl.Students   // здесь и так OrderBy не прошло потому что  cl.Students это List<string>
+                               orderby student   //  так OrderBy  прошло и ушло в результат
+                               select student;
+            //foreach (var studentName in studentNames.OrderBy(s=>s)) // а здесь OrderBy  прошло но естественно только на печать
+            //foreach (var studentName in studentNames) // 
+            //    Console.WriteLine(studentName);
+            //Console.WriteLine("-----------------------------");
+            return studentNames.ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
---
+++ Unit15/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit15
+{
+    // Модель автомобиля
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+    }
+
+    // Завод - изготовитель
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Список моделей
+            var cars = new List<Car>()
+            {
+                new Car() { Model  = "SX4", Manufacturer = "Suzuki"},
+                new Car() { Model  = "Grand Vitara", Manufacturer = "Suzuki"},
+                new Car() { Model  = "Jimny", Manufacturer = "Suzuki"},
+                new Car() { Model  = "Land Cruiser Prado", Manufacturer = "Toyota"},
+                new Car() { Model  = "Camry", Manufacturer = "Toyota"},
+                new Car() { Model  = "Polo", Manufacturer = "Volkswagen"},
+                new Car() { Model  = "Passat", Manufacturer = "Volkswagen"},
+            };
+
+            // Список автопроизводителей
+            var manufacturers = new List<Manufacturer>()
+            {
+                new Manufacturer() { Country = "Japan", Name = "Suzuki" },
+                new Manufacturer() { Country = "Japan", Name = "Toyota" },
+                new Manufacturer() { Country = "Germany", Name = "Volkswagen" },
+            };
+            var result = from car in cars // выберем машины
+                         join m in manufacturers on car.Manufacturer equals m.Name // соединим по общему ключу (имя производителя) с производителями
+                         select new //   спроецируем выборку в новую анонимную сущность
+                         {
+                             Name = car.Model,
+                             Manufacturer = m.Name,
+                             Country = m.Country
+                         };
+
+            // выведем результаты
+            foreach (var item in result.OrderBy(c => c.Country).ThenBy(m=>m.Manufacturer).ThenBy(n => n.Name))
+                Console.WriteLine($"{item.Name} - {item.Manufacturer} ({item.Country})");
+            Console.ReadKey();
+        }
+    }
+}
